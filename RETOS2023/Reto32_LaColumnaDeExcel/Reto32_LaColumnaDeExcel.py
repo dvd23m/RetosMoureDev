2c53c4b9d3aa8327ecdb1f4721068c0090f79b8a
David Molina Pulgarin <68005809+dvd23m@users.noreply.github.com>
2023-08-27 11:18:35 +0200

    Update Reto32_LaColumnaDeExcel.py

--- RETOS2023/Reto32_LaColumnaDeExcel/Reto32_LaColumnaDeExcel.py
+++ RETOS2023/Reto32_LaColumnaDeExcel/Reto32_LaColumnaDeExcel.py
@@ -1,11 +1,30 @@
 abc = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-cadena = 'XFC'
-celda = 0
-for exp, l in enumerate(reversed(cadena)):
-    celda += 26**exp * (abc.index(l) + 1)
-print(celda)
-    
 
+def calcularCelda(cadena):
+    '''
+        Calcula el número de la columna de Excel en función del nombre de la misma
+        Ejemplos:
+        Si el nombre es A la columna será 1 
+        Si el nombre es AA columna = 27
+        Si el nombre es BAA columna = 1379
+
+        Si el programa recibe la palabra 'salir' se detendrá
+
+        Params
+        cadena: String con el nombre de la columna
+        
+        Return:
+        No se retorna ningún valor. Se imprime directamente el resultado
+    '''
+    columna = 0
+    for exp, l in enumerate(reversed(cadena)):
+        columna += 26**exp * (abc.index(l) + 1)
+    print(f'La celda correspondiente a {cadena} es {columna}')
+
+while (cadena := input("Introduce una celda formato AAA: ")).isalpha() == False:
+    print("Solo se pueden introducir letras")
+if cadena != 'salir':
+    calcularCelda(cadena.upper())
 
 
 
