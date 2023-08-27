291a6755d70d041fc0d292b14a11b6810db53b5e
David Molina Pulgarin <68005809+dvd23m@users.noreply.github.com>
2023-08-27 18:12:20 +0200

    Add files via upload

---
+++ RETOS2023/Reto1_LenguajeHacker/Reto1_LenguajeHacker.py
@@ -0,0 +1,30 @@
+abc = {'a':'4',
+       'b':'|3',
+       'c':'[',
+       'd':')',
+       'e':'3',
+       'f':'|=',
+       'g':'&',
+       'h':'#',
+       'i':'1',
+       'j':',_|',
+       'k':'<|',
+       'l':'1',
+       'm':"/\\/\\",
+       'n':'^/',
+       'o':'0',
+       'p':'|*',
+       'q':'(_,)',
+       'r':'|2',
+       's':'5',
+       't':'7',
+       'u':'(_)',
+       'v':'\/',
+       'w':'\/\/',
+       'x':'><',
+       'y':'j',
+       'z':'2'}
+
+cadena = input("Introduce una frase: ")
+cadenaHack = [abc.get(l) if abc.get(l) is not None else '-' for l in cadena]
+print(cadenaHack)
